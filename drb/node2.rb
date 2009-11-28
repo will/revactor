707392bedd7df49eb6dc2e88d0438b553396da82
Will Leinweber <will@bitfission.com>
2009-11-28 12:47:14 -0600

    proxies now wait until they are remotely created and have been told they're created before finishing initializing

--- drb/node2.rb
+++ drb/node2.rb
@@ -3,11 +3,7 @@ require 'node_manager'
 manager = NodeManager.spawn(:node2)
 manager << :init
 
-# Actor[:manager] << T[:remote_create, :node_1, :rover, "Dog"]
 remote = RemoteActor.spawn("Dog", :node_1)
-Actor.sleep 10 # need to get proxy actors to hold messages until
-              # remote actor is up, so we don't need to sleep
-# Actor[:manager] << T[:remote_send, :node_1, :rover, :sit]
 remote << :sit
 
 loop { Actor.sleep 2 }
--- drb/node_manager.rb
+++ drb/node_manager.rb
@@ -7,6 +7,7 @@ class NodeManager
     @node_name = node_name
     @ts = TupleClient::get_ts
     @actors = {:manager => Actor.current}
+    @remote_proxies = {}
     Actor[:manager] = Actor.current
     receive_loop
   end
@@ -19,23 +20,27 @@ class NodeManager
   def get_messages
     take_all do |msg|
       actor, message = msg
+      puts "%%%% got: #{msg.inspect}"
       local_actor = @actors.fetch(actor) { |fail| puts "No such actor #{fail}"}
       local_actor << message if local_actor
     end
     
-    puts "[#{@node_name}]\t#{@actors.inspect}"
+    # puts "[#{@node_name}]\t#{@actors.inspect}"
     @actors[:manager] << :get_messages
-    Actor.sleep 2
+    Actor.sleep 0.1
   end
   call :get_messages, :when => :get_messages
   
-  def create_actor(_, actor_name, class_name)
+  def create_actor(_, actor_name, class_name, origin_node, key)
     @actors[actor_name] = Module::const_get(class_name).spawn
+    _remote_send(origin_node, :manager, [:actor_created, actor_name, key])
   end
   call :create_actor, :when => T[:create_actor]
   
-  def remote_create(_, node, actor_name, class_name)
-    _remote_send(node, :manager, [:create_actor, actor_name, class_name])
+  def remote_create(_, node, actor_name, class_name, proxy_actor, sender)
+    @remote_proxies[actor_name] = proxy_actor
+    key = sender[:key]
+    _remote_send(node, :manager, [:create_actor, actor_name, class_name, @node_name, key])
   end
   call :remote_create, :when => T[:remote_create]
   
@@ -43,6 +48,11 @@ class NodeManager
     _remote_send(node, actor, message)
   end
   call :remote_send, :when => T[:remote_send]
+  
+  def actor_created(_, actor_name, key)
+    @remote_proxies[actor_name] << T[key, true]
+  end
+  call :actor_created, :when => T[:actor_created]
   
   private
    
@@ -63,6 +73,7 @@ class NodeManager
   end
   
   def _remote_send(node, actor, message)
+    puts "**** sending #{node.inspect}\t#{actor.inspect}\t#{message.inspect}"
     @ts.write [node, [actor, message] ]
   end
 end
@@ -74,11 +85,11 @@ class RemoteActor
     @remote_node  = remote_node
     @actor_class  = actor_class
     @actor_name   = rand # TODO: replace with uuid?
-    Actor[:manager] << T[:remote_create, @remote_node, @actor_name, @actor_class]
+    Actor[:manager] >> T[:remote_create, @remote_node, @actor_name, @actor_class, Actor.current]
     receive_loop
   end
   
-  def forward(message)
+  def forward(message) # TODO: handle >> aka synchronous messages
     Actor[:manager] << T[:remote_send, @remote_node, @actor_name, message]
   end
   call :forward, :when => Object
